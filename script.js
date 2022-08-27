56f5121ac1472e6b2f88df7f88a0288b72850856
Friedrich <friedrich.ten.hagen@googlemail.com>
2022-08-27 22:31:39 +0200

    Add toggle button that changes CSS class of the list items on click

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const sect = document.querySelector("section"); 
-const buttonOne = document.querySelector("button");
 const input = document.querySelector("input"); 
 const list = document.querySelector("ul");
-
+const newList = document.querySelector("#newList");
 
 // allow entry of data via "Enter" and check to make sure string is not empty
 input.addEventListener("keypress", function(event) {
@@ -16,8 +15,19 @@ input.addEventListener("keypress", function(event) {
         }
     }
 });
+newList.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+      //  event.preventDefault();
+      if(newList.value === ""){
+          alert("Enter the name of your new List!");
+      }
+      else{
+          createNewList();
+      }
+  }
+});
 
-
+/*
 function enterInput(){
   const button = document.createElement("button");
   let inputText = document.querySelector("input").value;
@@ -35,3 +45,55 @@ function enterInput(){
     sect.removeChild(button);
   });
 }
+*/
+function enterInput(){
+  const listItem = document.createElement("li");
+  let inputText = document.querySelector("input").value;
+
+  //set button to input text, add class and id, append to doc
+  listItem.textContent = inputText;
+  listItem.setAttribute('class', 'highlight');
+  list.appendChild(listItem);
+
+  //clear input field
+  let inputContent = document.querySelector("input");
+  inputContent.value = "";
+
+  listItem.addEventListener('dblclick', (e) => {
+      list.removeChild(listItem);
+  });
+  listItem.addEventListener('click', (e) => {
+    listItem.classList.toggle('highlight2');
+  });
+
+}
+
+function createNewList(){
+  const newUl = document.createElement("ul");
+  const varLists = document.querySelector(".varLists");
+
+  const listItem = document.createElement("li");
+  listItem.textContent = document.querySelector("#newList").value;
+  newUl.appendChild(listItem);
+  
+  
+  varLists.appendChild(newUl);
+}
+
+
+/* use toggle to switch CSS classes
+div.classList.add('new');                                      
+// adds class "new" to your new div
+
+div.classList.remove('new');                                   
+// removes "new" class from div
+
+div.classList.toggle('active');                                
+// if div doesn't have class "active" then add it, or if
+// it does, then remove it
+
+
+
+use nodelist to access different list items?
+
+*/
